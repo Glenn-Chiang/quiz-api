3b0630eab3d3ac7bb9bc1a8bab7e46bd71ec901b
Glenn <glennchiang24@gmail.com>
2024-02-25 20:53:44 +0800

    Add error handler

---
+++ app/routes/errors.py
@@ -0,0 +1,19 @@
+from werkzeug.http import HTTP_STATUS_CODES
+from werkzeug.exceptions import HTTPException
+from app import app
+
+
+def error_response(status_code: int, message: str = None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    return payload, status_code
+
+
+def bad_request(message: str):
+    return error_response(status_code=400, message=message)
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(error: HTTPException):
+    return error_response(error.code, message=error.description)
