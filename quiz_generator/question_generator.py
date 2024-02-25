8e6c2c5aa2e4d703718264d28becfe4010c0d8a4
Glenn <glennchiang24@gmail.com>
2024-02-25 23:07:16 +0800

    Add validation for AI output using json schema

--- quiz_generator/question_generator.py
+++ quiz_generator/question_generator.py
@@ -1,18 +1,22 @@
 import os
 import json
+import jsonschema
+from jsonschema.exceptions import ValidationError
 import google.generativeai as genai
 from dotenv import load_dotenv
 load_dotenv()
 
 
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+SAMPLE_OUTPUT_FILE = 'sample_output.json'
+OUTPUT_SCHEMA_FILE = 'output_schema.json'
 
 
 def generate_questions(subject: str, question_count: int, choice_count: int):
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
 
-    with open('quiz_sample.json', 'r') as file:
+    with open(SAMPLE_OUTPUT_FILE, 'r') as file:
         quiz_sample = json.dumps(json.load(file))
 
     prompt = (f"Given the subject below, generate a series of {question_count} quiz questions on the subject."
@@ -24,11 +28,25 @@ def generate_questions(subject: str, question_count: int, choice_count: int):
     try:
         response = model.generate_content(prompt)
         response_text = response.text
-        questions = json.loads(response_text)
-        return questions
     except Exception as error:
         print(f'Error generating response from gemini:', error)
         return
+
+    output_json = json.loads(response_text)
+    # Check if gemini returned output in correct format
+    try:
+        validate_output(output_json)
+        return output_json
+    except ValidationError as error:
+        print('Invalid output:', error)
+        return
+
+
+def validate_output(output):
+    with open(OUTPUT_SCHEMA_FILE, 'r') as schema_file:
+        schema = json.load(schema_file)
+    jsonschema.validate(instance=output, schema=schema)
+
 
 if __name__ == '__main__':
     questions = generate_questions(subject='computer networking',
