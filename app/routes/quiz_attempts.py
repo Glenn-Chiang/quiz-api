80ebad40848796d202ee8229971809c26e723385
Glenn <glennchiang24@gmail.com>
2024-03-03 13:21:38 +0800

    Add pagination for get_user_attempts endpoint

--- app/routes/quiz_attempts.py
+++ app/routes/quiz_attempts.py
@@ -2,6 +2,8 @@ from app import app, db
 from app.models import QuizAttempt
 from flask import request
 from app.routes.errors import error_response
+from sqlalchemy import select
+
 
 @app.get('/attempts')
 def get_all_attempts():
@@ -18,9 +20,14 @@ def get_quiz_attempts(quiz_id: int):
     return [attempt.to_dict() for attempt in QuizAttempt.query.filter(QuizAttempt.quiz_id == quiz_id).all()]
 
 
+# Get all quiz attempts made by this user
 @app.get('/users/<int:user_id>/attempts')
 def get_user_attempts(user_id: int):
-    return [attempt.to_dict() for attempt in QuizAttempt.query.filter(QuizAttempt.user_id == user_id).all()]
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 20, type=int), 100)
+    return QuizAttempt.to_collection_dict(select(QuizAttempt).where(QuizAttempt.user_id == user_id),
+                                          page=page, per_page=per_page,
+                                          endpoint='get_user_attempts', user_id=user_id)
 
 
 # Should be called when a user starts to attempt a quiz
@@ -29,7 +36,7 @@ def add_quiz_attempt(quiz_id: int):
     user_id = request.args.get('user_id', type=int)
     if not user_id:
         return error_response(status_code=400, message='Missing or invalid user_id')
-    
+
     quiz_attempt = QuizAttempt(quiz_id=quiz_id, user_id=user_id)
     db.session.add(quiz_attempt)
     db.session.commit()
